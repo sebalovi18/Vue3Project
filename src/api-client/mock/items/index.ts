4fd7ff09f25e0704778bf9fdb222c8c9e998b72d
Sebastian <sebalovi12@gmail.com>
2021-08-09 15:41:12 -0300

    Unit 7

---
+++ src/api-client/mock/items/index.ts
@@ -0,0 +1,12 @@
+import {
+  ItemsApiClientUrlsInterface,
+  ItemsApiClientInterface,
+  ItemsApiClientModel,
+} from '@/models/api-client/items';
+
+const urls: ItemsApiClientUrlsInterface = {
+  fetchItems: '/static/data/items.json',
+};
+const itemsApiClient: ItemsApiClientInterface = new ItemsApiClientModel(urls);
+
+export default itemsApiClient;
