0e1eb5323d05a8635123e8b22222b56b5f22ccc0
Sebastian <sebalovi12@gmail.com>
2021-08-09 14:33:07 -0300

    Unit 6

--- src/components/items/ItemsList.component.vue
+++ src/components/items/ItemsList.component.vue
@@ -1,27 +1,37 @@
 <template>
   <div>
-    <h3>Items:</h3>
-    <ul>
-      <li v-for="item in items" :key="item.id" @click="onItemSelect(item)">
-        {{ item.name }}
-      </li>
+    <h3>Items - loading: {{ loading }}</h3>
+    <Loader v-show="loading" />
+    <ul v-show="!loading">
+      <ItemComponent v-for="item in items" 
+        :key="item.id" 
+        :model="item" 
+        @select="onItemSelect" />
     </ul>
   </div>
 </template>
 <script lang="ts">
   import { defineComponent, PropType } from 'vue'
   import { ItemInterface } from '@/models/items/Item.interface'
+  import ItemComponent from './children/Item.component.vue'
+  import Loader from '@/components/shared/Loader.component.vue'
 
   export default defineComponent({
+    components: { 
+      ItemComponent,
+      Loader
+    }, 
     props: {
       items: {
         type: Array as PropType<ItemInterface[]>
+      },
+      loading: {
+        type: Boolean
       }
     },
-    setup() {
+    setup(props, {emit}) {
       const onItemSelect = (item: ItemInterface) => {
-        item.selected = !item.selected
-        console.log('onItemSelect', item.id, item.selected)
+        emit('selectItem', item)
       }
 
       return {
@@ -30,3 +40,14 @@
     }
   })
 </script>
+
+<style lang="scss">
+  ul {
+    list-style-type: none;
+    margin-block-start: 0;
+    margin-block-end: 0;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    padding-inline-start: 0px;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,38 +1,47 @@
 <template>
  <div class="home">
-  <ItemsListComponent :items="items" />
+  <ItemsListComponent 
+    :items="items" 
+    :loading="loading" 
+    @selectItem="onSelectItem" 
+  />
  </div>
 </template>
 <script lang="ts">
-  import { defineComponent } from 'vue'
+  import { defineComponent, computed, onMounted } from 'vue'
+  import store from '@/store'
   import ItemsListComponent from '@/components/items/ItemsList.component.vue'
   import { ItemInterface } from '@/models/items/Item.interface'
 
   export default defineComponent({
-    name: 'Home',
+    name: 'Home', 
     components: {
       ItemsListComponent
     },
     setup() {
-      const items: ItemInterface[] = [
-	  {
-        id: 1,
-        name: 'Item 1',
-        selected: false
-      }, 
-	  {
-        id: 2,
-        name: 'Item 2',
-        selected: false
-      }, 
-	  {
-        id: 3,
-        name: 'Item 3',
-        selected: false
-      }]
+      const items = computed(() => {
+        return store.state.items
+      })
+
+      const loading = computed(() => {
+        return store.state.loading
+      })
+
+      onMounted(() => {
+        store.dispatch('loadItems')
+      })
+
+      const onSelectItem = (item: ItemInterface) => {
+        store.dispatch('selectItem', {
+          id: item.id,
+          selected: !item.selected
+        })
+      }
 
       return {
-        items
+        items,
+        loading,
+        onSelectItem
       }
     }
   })
